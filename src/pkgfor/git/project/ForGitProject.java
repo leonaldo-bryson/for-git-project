c156fa87945cd47dca04d9f32d615376ead3eb37
leo <leoifeanyi4@gmail.com>
2019-07-14 21:42:30 +0100

    last trial

---
+++ src/pkgfor/git/project/ForGitProject.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfor.git.project;
+
+/**
+ *
+ * @author LEO
+ */import java.util.Scanner;
+public class ForGitProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a= 6;
+        int b = 5;
+        int sum =a+b;
+                System.out.println(sum);
+        // TODO code application logic here
+    }
+    
+}
